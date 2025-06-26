1edf2267e504f6827a2f56357fb90334f06dab8d
Dimly82 <pdimochka@gmail.com>
2025-06-26 11:33:49 +0300

    reminder base, github workflow

---
+++ GamerCompanion/Models/Reminder.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace GamerCompanion.Models;
+
+public class Reminder : INotifyPropertyChanged {
+    public string Text { get; set; }
+
+    public DateTime TargetTime { get; set; }
+
+    public int TimeMinutes => Math.Max(0, (int)(TargetTime - DateTime.Now).TotalMinutes);
+
+    public bool Repeating { get; set; }
+
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    public void RaiseTimeChanged() {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TimeMinutes)));
+    }
+}
---
+++ GamerCompanion/ViewModels/ReminderViewModel.cs
@@ -0,0 +1,50 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using GamerCompanion.Models;
+using System.Collections.ObjectModel;
+using System.Windows.Threading;
+
+namespace GamerCompanion.ViewModels;
+
+public partial class ReminderViewModel : ObservableObject {
+    public ObservableCollection<Reminder> Reminders { get; } = new();
+
+    private DispatcherTimer _reminderTimer;
+
+    [ObservableProperty]
+    private string newReminderText = "";
+    [ObservableProperty]
+    private int newReminderTimeMinutes = 10;
+    [ObservableProperty]
+    private bool newReminderRepeating = false;
+
+    public ReminderViewModel() {
+        _reminderTimer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(30) };
+        _reminderTimer.Tick += (s, e) => UpdateReminderTimes();
+        _reminderTimer.Start();
+    }
+
+    private void UpdateReminderTimes() {
+        foreach (var reminder in Reminders)
+            reminder.RaiseTimeChanged();
+    }
+
+    [RelayCommand]
+    private void AddReminder() {
+        if (string.IsNullOrWhiteSpace(NewReminderText) || NewReminderTimeMinutes <= 0)
+            return;
+
+        var newReminder = new Reminder { Text = NewReminderText, TargetTime = DateTime.Now + TimeSpan.FromMinutes(NewReminderTimeMinutes), Repeating = NewReminderRepeating };
+        Reminders.Add(newReminder);
+
+        NewReminderText = "";
+        NewReminderTimeMinutes = 10;
+        NewReminderRepeating = false;
+    }
+
+    [RelayCommand]
+    private void RemoveReminder(Reminder reminder) {
+        if (reminder != null)
+            Reminders.Remove(reminder);
+    }
+}
